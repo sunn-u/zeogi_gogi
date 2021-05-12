bcd30c5a63c916abcdaf7f6d524be757198feffd
jjung <rurounida88@gmail.com>
2021-05-12 17:04:01 +0900

    Change train.py file

--- train.py
+++ train.py
@@ -13,18 +13,20 @@ def train():
     model.train()
 
     classes = ('fear', 'happy', 'sad', 'neutral', 'disgust', 'surprise', 'angry')
+    import torch.optim as optim
+    import time
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(),lr=0.0001)
-    #device = torch.device("cuda:0")
+    optimizer = optim.Adam(model.parameters(),lr=0.00001)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    #cuda = device.type != 'cpu'
+
 
     start_time = time.time()
     for epoch in range(2):  # loop over the dataset multiple times
-        running_loss = 0.0
-        #dataiter = iter(trainloader)
-        #images, labels = dataiter.next()
+        trainLoss = 0.0
+        trainSize = 0
+        trainCorrect = 0
+        
         for i, data in enumerate(trainloader, 0):
             # get the inputs
             inputs, labels = data
@@ -32,9 +34,9 @@ def train():
             # zero the parameter gradients
             optimizer.zero_grad()
 
-
             # forward + backward + optimize
             model = model.float()
+            #outputs,f = model(inputs)
             model = model.cuda()
             outputs = model(inputs)
 
@@ -42,20 +44,61 @@ def train():
             loss.backward()
             optimizer.step()
 
+            trainLoss += loss.item()
+
+            predicted = outputs.max(1)[1]
+            #print(predicted)
+            #print(labels)
+      
+            trainSize += labels.shape[0]
+            trainCorrect += predicted.eq(labels.view_as(predicted)).sum().item()
+            trainAccuracy = (trainCorrect / trainSize) * 100
+
+
+            '''
             if(loss.item() > 1000):
                 print(loss.item())
                 for param in model.parameters():
                     print(param.data)
+            '''
             # print statistics
-            running_loss += loss.item()
+            #trainLoss += loss.item()
             if i % 50 == 49:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 50))
-                running_loss = 0.0
+                torch.save(model, '/content/drive/MyDrive/vgg16_epoch{}_batch{}_accuracy{:.3f}.pt'.format(epoch+1, i+1, trainAccuracy))
+                print('epoch {} batch {} train_loss {}  accuracy {}'.format(epoch+1, i+1, trainLoss / 50, trainAccuracy))
+                #trainLoss = 0.0   # 이걸 해야할거같은..느낌...?
 
-    torch.save(model, '/content/drive/MyDrive/vgg16.pt')
-    torch.save(model.state_dict(), '/content/drive/MyDrive/vgg16.pth')
+
+        if i%5 == 0:
+            with torch.no_grad():
+                valLoss = 0
+                valSize = 0
+                valCorrect = 0
+
+                for j, data in enumerate(testloader):
+                    # get the inputs
+                    inputs, labels = data
+                    inputs, labels = inputs.to(device), labels.to(device)
+
+                    # forward + backward + optimize
+                    model = model.float()
+                    #outputs,f = model(inputs)
+                    model = model.cuda()
+                    outputs = model(inputs)
+
+                    valLoss += criterion(outputs, labels).item()
+
+                    predicted = outputs.max(1)[1]
+                    valSize += labels.shape[0]
+                    valCorrect += predicted.eq(labels.view_as(predicted)).sum().item()
+                    valAccuracy = (valCorrect / valSize) * 100
+                
+                print('validation_loss {}'.format(valLoss))
+
+
+
+    #torch.save(model, '/content/drive/MyDrive/vgg16.pt')
+    #torch.save(model.state_dict(), '/content/drive/MyDrive/vgg16.pth')
     print(time.time()-start_time)
     print('Finished Training')
 
-    
