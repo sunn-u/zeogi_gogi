552a2c02bfc2f6583a0a40101697aa1b8de1af51
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2021-05-13 18:21:23 +0900

    Update vggModule.py

--- vggModule.py
+++ vggModule.py
@@ -7,10 +7,10 @@ import torch.nn.functional as F
 from configs import Configs
 
 
-class VGG(nn.Module):
+class Vgg(nn.Module):
 
     def __init__(self, conv, num_classes=100, init_weights=True):
-        super(VGG, self).__init__()  # pytorch에서 class 형태의 모델은 항상 nn.Module을 상속받아야 하며, 
+        super(Vgg, self).__init__()  # pytorch에서 class 형태의 모델은 항상 nn.Module을 상속받아야 하며, 
                                      # super(모델명, self).init()을 통해 nn.Module.init()을 실행시키는 코드가 필요
 
         self.features = conv    # features
