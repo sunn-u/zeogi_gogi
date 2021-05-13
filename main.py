9a1773f925db06e4519ec6cf14ee297cbe0faedc
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2021-05-13 18:05:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,27 +12,27 @@ from train import train
 
 
 def main():
-	make_train_val_set(Configs.data_root)
-	dataiter = iter(trainloader)
-	images, labels = dataiter.next()
+    make_train_val_set(Configs.data_root)
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
 
-	cfg = {  # 8 + 3 =11 == vgg11
-    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    # 10 + 3 = vgg 13
-    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    # 13 + 3 = vgg 16
-    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
-    # 16 +3 =vgg 19
-    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
-	  }
+    cfg = {  # 8 + 3 =11 == vgg11
+	    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+	    # 10 + 3 = vgg 13
+	    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+	    # 13 + 3 = vgg 16
+	    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+	    # 16 +3 =vgg 19
+	    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
+	}
     conv = make_layers(cfg[Configs.model_config], batch_norm=True)
     model = Vgg(conv, num_classes=Configs.class_num, init_weights=True)
     print(model)
 
-	train(model, trainloader, testloader)
+    train(model, trainloader, testloader)
 
 
 
 if __name__ == '__main__':
 	
-	main()
+    main()
